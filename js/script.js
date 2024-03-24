ead408882773d16f49cb1e74aa9e8a00fe4e7629
Aarthi <136609619+Aarthi0514@users.noreply.github.com>
2024-03-24 14:47:30 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,76 @@
+//LOGO GIF//
+window.onload = function() {
+  var gif = document.getElementById('gifImage');
+
+  // Check if the GIF has been played before
+  var hasPlayed = localStorage.getItem('hasPlayed');
+  
+  if (!hasPlayed) {
+    // Play the GIF once
+    gif.src = gif.src; // Restart the GIF by resetting its src attribute
+    localStorage.setItem('hasPlayed', true); // Set hasPlayed to true in localStorage
+  }
+};
+
+//MAIN SECTION//
+var htmlEditor = ace.edit("html");
+htmlEditor.setTheme("ace/theme/cobalt");
+htmlEditor.session.setMode("ace/mode/html");
+htmlEditor.resize();
+htmlEditor.setHighlightActiveLine(false);
+
+var cssEditor = ace.edit("css");
+cssEditor.setTheme("ace/theme/cobalt");
+cssEditor.session.setMode("ace/mode/css");
+cssEditor.resize();
+cssEditor.setHighlightActiveLine(false);
+
+var jsEditor = ace.edit("js");
+jsEditor.setTheme("ace/theme/cobalt");
+jsEditor.session.setMode("ace/mode/javascript");
+jsEditor.resize();
+jsEditor.setHighlightActiveLine(false);
+
+function compiler() {
+  var htmlValue = htmlEditor.getValue();
+  var cssValue = cssEditor.getValue();
+  var jsValue = jsEditor.getValue();
+  var result = document.getElementById("result").contentWindow.document;
+
+  result.open();
+  result.writeln(
+    "<style>" +
+    cssValue +
+    "</style>" +
+    htmlValue +
+    "<script>" +
+    jsValue +
+    "</script>"
+  );
+  result.close();
+}
+
+var allButtons = document.querySelectorAll("#button-wrapper button");
+var allPanels = document.querySelectorAll("#ide-container .panel-wrapper");
+
+function switchPanel(panelIndex) {
+  switcher(panelIndex);
+}
+
+switchPanel(0);
+
+function runEdit(panelIndex) {
+  switcher(panelIndex);
+  compiler();
+}
+
+function switcher(panelIndex) {
+  allButtons.forEach(function (node) {
+    node.style.background = "";
+  });
+  allButtons[panelIndex].style.background = " radial-gradient(circle, #869191, #789191, #689191, #569091, #419091)";
+  allPanels.forEach(function (node) {
+    node.style.display = "none";
+  });
+  allPanels[panelIndex].style.display = "block";
+}
